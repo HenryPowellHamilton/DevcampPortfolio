126ac778a4572269981e69652adfb8eeef6f3b0a
slcwestdevcamp <slcwest@devcamp.com>
2017-06-26 14:52:08 +0000

    intgrated nothing

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,9 @@ end
             thumb_image: "http://via.placeholder.com/350x200"
             )
         end
+        
+    3.times do ||technology
+            Portfolio.last.technologies.create!(
+            name: "technology #{technology}"
+            )
+    end
