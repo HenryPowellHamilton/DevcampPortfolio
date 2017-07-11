8d6a5aba1fb0814b88ddbbf7da0159267fb3d4da
slcwestdevcamp <slcwest@devcamp.com>
2017-07-11 16:00:38 +0000

    added my gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,12 @@ module ApplicationHelper
       link_to "Logout", destroy_user_session_path, method: :delete
     else
       (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe + 
-      "<hr>".html_safe +
+      "<br>".html_safe +
       (link_to "Login", new_user_session_path)
     end
   end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  end
 end
-
-
-
