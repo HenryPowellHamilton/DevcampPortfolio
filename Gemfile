84201dead1e0ab1004bec6de3ebf4f1d35fe9b45
slcwestdevcamp <slcwest@devcamp.com>
2017-07-27 14:11:25 +0000

    made nav footer and masthead partials

--- Gemfile
+++ Gemfile
@@ -51,8 +51,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
-gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
-gem 'jquery-rails'
+gem 'bootstrap', '~> 4.0.0.alpha6'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'petergate', '~> 1.7', '>= 1.7.5'
 gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 gem 'kaminari', '~> 1.0', '>= 1.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :users
-  resources :portfolios
+  resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'pages#home'
+  
+  root to: 'pages#home'
 end
